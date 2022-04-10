d7eefcdfd588099526263703dbd9f43638b05f99
Farzana045 <Farzana045@gmail.com>
2022-04-10 08:43:38 -0500

    Java_midterm

--- src/string_problems/DuplicateWord.java
+++ src/string_problems/DuplicateWord.java
@@ -2,16 +2,35 @@ package string_problems;
 
 public class DuplicateWord {
 
-    /** INSTRUCTIONS
+    /**
+     * INSTRUCTIONS
      * Write a java program to find duplicate words and the number of occurrences of those words in the given string.
      * Also, find the average length of all the words
      */
 
     public static void main(String[] args) {
+
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
 
-        // Implement here
+        int count;
 
+        st = st.toLowerCase();   //Converts the string into lowercase
+
+        String words[] = st.split(" "); //Split the string into words using built-in function
+
+        System.out.println("Duplicate words in a given string : ");
+
+          for(int i = 0; i < words.length; i++) {
+              count = 1;
+                   for(int j = i+1; j < words.length; j++) {
+                        if(words[i].equals(words[j])) {
+                           count++;
+
+                    words[j] = "0";  //Set words[j] to 0 to avoid printing visited word
+                }
+            }
+            if(count > 1 && words[i] != "0")  //Displays the duplicate word if count is greater than 1
+                System.out.println(words[i]);
+        }
     }
-
 }
--- src/string_problems/Permutation.java
+++ src/string_problems/Permutation.java
@@ -8,8 +8,31 @@ public class Permutation {
      * e.g. -  "ABC" = "ABC" "ACB" "BAC" "BCA" "CAB" "CBA"
      */
 
+    static void printPermutn (String str, String ans)  // Function to print all the permutations of str
+    {
+
+        if (str.length() == 0)   // If string is empty
+
+        {
+            System.out.print(ans + " ");
+            return;
+        }
+
+        for (int i = 0; i < str.length(); i++)
+
+        {
+
+            char ch = str.charAt(i); // ith character of str
+
+            String ros = str.substring(0, i) + str.substring(i + 1);  // Rest of the string after excluding   // the ith character
+
+            printPermutn(ros, ans + ch);  // Recursive call
+        }
+    }
     public static void main(String[] args) {
 
+        String s = "ABC";
+        printPermutn(s, " ");
     }
-    // Implement Here
 }
+
